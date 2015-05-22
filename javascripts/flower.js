fb9d37531d206744d1fb6243f0ad934cf25f983e
Jonathan Meyers <jmeyers44@gmail.com>
2015-05-22 10:12:27 -0400

    Site updated at 2015-05-22 14:12:27 UTC

--- javascripts/flower.js
+++ javascripts/flower.js
@@ -35,25 +35,21 @@ $( document ).ready(function() {
 
 
 
-// var blob = new Blob([document.getElementById('v0')]);
-// var obj_url = window.URL.createObjectURL(blob);
-// $('source').attr('src',obj_url)
+
 var vid = document.getElementById('v0');
 
-
-
-// // // pause video on load
+// pause video on load
 vid.pause();
  
-// // // pause video on document scroll (stops autoplay once scroll started)
+// pause video on document scroll (stops autoplay once scroll started)
 $('#vid-container').onscroll = function(){
     vid.pause();
 };
 
-// // // refresh video frames on interval for smoother playback
-// // // dividing the totalTime scrubTime calculation by 25 determines 
-// // // how many frames will be covered in the span of a 'normal' scroll
-// // // the lower the number, the more frames will be covered in a scroll
+// refresh video frames on interval for smoother playback
+// dividing the totalTime scrubTime calculation by 25 determines 
+// how many frames will be covered in the span of a 'normal' scroll
+// the lower the number, the more frames will be covered in a scroll
 setInterval(function(){
     var totalTime = 290
     var scrubTimeSelect = $('#vid-container-2').position();
