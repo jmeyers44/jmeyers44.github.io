95f5bbdcc25277792ac2b70f2d163256bf2caabb
Jonathan Meyers <jmeyers44@gmail.com>
2015-05-21 23:56:31 -0400

    Site updated at 2015-05-22 03:56:31 UTC

--- javascripts/flower.js
+++ javascripts/flower.js
@@ -1,7 +1,28 @@
 $( document ).ready(function() {
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/images/flower-slow-bloom-4k.mp4', true);
+  xhr.responseType = 'blob';
+
+  xhr.onload = function(e) {
+    if (this.status == 200) {
+      // Note: .response instead of .responseText
+      var blob = this.response;
+      var vid_url = URL.createObjectURL(blob);
+      $('video').attr('src',vid_url)
+      var video = $('video');
+      video[0].onload = function(){
+        debugger;
+        URL.revokeObjectURL(vid_url)
+      }
+      video[0].play()
+    }
+  };
+
+  xhr.send();
     console.log( "ready!" );
 });
-$(window).bind("load", function() {
+
    // code here
 
 
@@ -9,25 +30,30 @@ $(window).bind("load", function() {
 // var selectVid = document.getElementById('v0');
 //convert to blob
 // var vid = document.getElementById('v0');
-var blob = new Blob([document.getElementById('v0')]);
-var obj_url = window.URL.createObjectURL(blob);
-$('source').attr('src',obj_url)
+
+
+
+
+
+// var blob = new Blob([document.getElementById('v0')]);
+// var obj_url = window.URL.createObjectURL(blob);
+// $('source').attr('src',obj_url)
 var vid = document.getElementById('v0');
 
 
 
-// pause video on load
+// // // pause video on load
 vid.pause();
  
-// pause video on document scroll (stops autoplay once scroll started)
+// // // pause video on document scroll (stops autoplay once scroll started)
 $('#vid-container').onscroll = function(){
     vid.pause();
 };
 
-// refresh video frames on interval for smoother playback
-// dividing the totalTime scrubTime calculation by 25 determines 
-// how many frames will be covered in the span of a 'normal' scroll
-// the lower the number, the more frames will be covered in a scroll
+// // // refresh video frames on interval for smoother playback
+// // // dividing the totalTime scrubTime calculation by 25 determines 
+// // // how many frames will be covered in the span of a 'normal' scroll
+// // // the lower the number, the more frames will be covered in a scroll
 setInterval(function(){
     var totalTime = 290
     var scrubTimeSelect = $('#vid-container-2').position();
@@ -35,4 +61,3 @@ setInterval(function(){
     vid.currentTime = eval(totalTime+ " " + "-" + " " + scrubTime) /25;
 }, 40);
 
-});
